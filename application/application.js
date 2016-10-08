74d6722ddaae5b30dc07873ca0e9e07c7817f5b2
Alexey <kupriyanov_95@inbox.ru>
2016-10-08 19:10:22 +0300

    ))

--- application/application.js
+++ application/application.js
@@ -1,7 +1,19 @@
+//точка входа
 var express = require("express");
+var bodyparser = require("body-parser");
+var favicon = require("express-favicon");
+var static = require("express-static");
+var path = require("path");
 var app = express();
 
-app.get("/", function (req, res) {
+//app.use(favicon()); // отдаем стандартную фавиконку, можем здесь же свою задать
+//app.use(express.logger('dev')); // выводим все запросы со статусами в консоль
+app.use(bodyparser.json()); // стандартный модуль, для парсинга JSON в запросах
+//app.use(express.methodOverride()); // поддержка put и delete
+//app.use(app.router); // модуль для простого задания обработчиков путей
+app.use(express.static(path.join(__dirname, "public"))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
+
+app.get("/api", function (req, res) {
     res.end("Big brother is watching you...");
 });
 
