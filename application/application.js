a57293bb7ec6de08459fe2b6d20041aa50b6e84e
Alexey <kupriyanov_95@inbox.ru>
2016-10-09 14:16:27 +0300

    added new station

--- application/application.js
+++ application/application.js
@@ -2,38 +2,58 @@
 var express = require("express");
 var bodyparser = require("body-parser");
 //var favicon = require("express-favicon");
-//var express_static = require("express-static");
 var path = require("path");
 var log = require('./libs/logger')(module);
 var morgan = require('morgan');
 var app = express();
+var StationModel = require('./libs/mongoose').StationModel;
 
 //module using
 //app.use(favicon()); // отдаем стандартную фавиконку, можем здесь же свою задать
-//app.use(express.logger('dev')); // выводим все запросы со статусами в консоль
 app.use(morgan("dev"));
 app.use(bodyparser.json()); // стандартный модуль, для парсинга JSON в запросах
 //app.use(express.methodOverride()); // поддержка put и delete
-//app.use(app.router); // модуль для простого задания обработчиков путей
 app.use(express.static(path.join(__dirname, "public"))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
 
-//error handlers
-app.use(function(req, res, next){
-    res.status(404);
-    log.debug('Not found URL: %s',req.url);
-    res.send({ error: 'Not found' });
-    return;
-});
-app.use(function(err, req, res, next){
-    res.status(err.status || 500);
-    log.error('Internal error(%d): %s',res.statusCode,err.message);
-    res.send({ error: err.message });
-    return;
-});
 
 //routes
 app.get("/api", function (req, res) {
     res.end("Big brother is watching you...");
+});
+
+app.get('/api/stations', function(req, res) {
+    return StationModel.find(function (err, stations) {
+        if (!err) {
+            return res.send(stations);
+        } else {
+            res.statusCode = 500;
+            log.error('Internal error(%d): %s', res.statusCode, err.message);
+            return res.send({error: 'Server error'});
+        }
+    });
+});
+
+app.post('/api/stations', function(req, res) {
+    var station = new StationModel({
+        title: req.body.title
+    });
+
+    station.save(function (err) {
+        if (!err) {
+            log.info("station created");
+            return res.send({ status: 'OK'});
+        } else {
+            console.log(err);
+            if(err.name == 'ValidationError') {
+                res.statusCode = 400;
+                res.send({ error: 'Validation error' });
+            } else {
+                res.statusCode = 500;
+                res.send({ error: 'Server error' });
+            }
+            log.error('Internal error(%d): %s',res.statusCode,err.message);
+        }
+    });
 });
 
 //server launch
