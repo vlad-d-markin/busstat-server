98dbd6bc191bbdf82f3cfb1ecd506e58a0b48c19
Vlad Markin <vlad.d.markin@gmail.com>
2016-11-11 13:33:03 +0300

    Major milestone: tits are back

--- admin-tool/app/about.jsx
+++ admin-tool/app/about.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import {ListGroup, ListGroupItem} from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 
 
 export default class About extends React.Component {
   render() {
     return (
-      <p>About. Coming soon.</p>
+    <Carousel>
+        <Carousel.Item>
+            <img width={900} height={500} alt="900x500" src="https://pp.vk.me/c425325/v425325006/4c5b/_s_0CtAMdXI.jpg"/>
+            <Carousel.Caption>
+                <h3>Больше сисек</h3>
+                <p>Богу сисек</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img width={900} height={500} alt="900x500" src="https://pp.vk.me/c616731/v616731543/16cec/pR_kiGm3cF4.jpg"/>
+            <Carousel.Caption>
+                <h3></h3>
+                <p></p>
+            </Carousel.Caption>
+        </Carousel.Item>
+    </Carousel>
     );
   }
 }
